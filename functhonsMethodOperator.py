1d792962d60ede2116f0f73183221331b9ba8ba4
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-12-12 20:56:43 +0600

    add functions Method and Operator

---
+++ functhonsMethodOperator.py
@@ -0,0 +1,16 @@
+course = 'Python for Beginners'
+# print(len(course))
+# print(course.upper())
+# print(course)
+# print(course.lower())
+# print(course.find('P')) for find the index of element in a variable
+# print(course.find('o'))
+# print(course.find('M')) return -1 which element doesn't exit on variable
+# print(course.find('O')) return -1 because find() is case sensative
+# print(course.find('p'))
+# print(course.find('Beginners'))
+# print(course.replace('Beginners', 'Absolute Beginners'))
+# print(course)
+# print(course.replace('python', 'Jython'))
+# print('Python' in course) in operator for boolean value
+print('python' in course)
