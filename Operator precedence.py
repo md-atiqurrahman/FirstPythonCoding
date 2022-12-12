6313f84aad89f5d0182dc12f0d89f987e6de3424
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-12-12 23:42:47 +0600

    change value of x in operator precedence

--- Operator precedence.py
+++ Operator precedence.py
@@ -3,5 +3,5 @@
 # third priority goes to multiplication or division operatior * or /
 # And the last priority goes to addition or subtraction operator + or -
 
-x = (10 + 3) * 3 ** 2
+x = (2 + 3 ) * 10 - 3
 print(x)
