9858fd70e699e69f6703785203b056dde60eacd5
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-12-13 00:04:38 +0600

    add math functions

---
+++ Math functions.py
@@ -0,0 +1,15 @@
+import math
+
+x = 2.9
+# print(round(x)) return a value of x in a round figure
+x = - 2.9
+# print(abs(x)) abs short form of absolute which return a positive value of any number
+
+# for calculate complex math in python we need to import math module
+y = 3.888
+value = math.ceil(y)
+# print(value)
+
+z = 5.9999
+result = math.floor(z)
+print(result)
