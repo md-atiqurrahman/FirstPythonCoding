24afedf3804d935a90cc3c7f632c1164cf40cefa
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-12-12 23:08:31 +0600

    add Arithmetic operator

---
+++ Arithmetic operator.py
@@ -0,0 +1,30 @@
+# print(10 + 3) + this is addition operator
+# print(10 - 3) - this is subtraction operator
+# print(10 * 3) * this is multiplication operator
+# print(10 / 3) / this is division operator return floating number
+# print(10 // 3) // this is also division operator which return integer number
+# print(10 % 3) % this is modulis operator which return percent value
+# print(10 ** 3) ** this is power operator
+
+# x = 10
+# x = x + 3
+# print(x)
+
+# augment assignment operator(short form of + , - , * , /)
+x = 10
+x += 3
+# print(x)
+x = 20
+x -= 6
+# print(x)
+x = 25
+x *= 4
+# print(x)
+
+x = 75
+x /= 5
+# print(x) which return floating number
+
+x = 75
+x //= 5
+# print(x) which return integar number
