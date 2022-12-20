1c57ba6fc735d6541a9b0bf887206f010dfdf553
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-12-20 20:16:51 +0600

    add if statements

---
+++ If statements.py
@@ -0,0 +1,21 @@
+# is_hot = False
+# is_cold = False
+#
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink a plenty of water")
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm clothes")
+# else:
+#     print("It's a lovely day")
+# print("Enjoy your day")
+
+house_price = 1000000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = 0.1 * house_price
+else:
+    down_payment = 0.2 * house_price
+print(f'Down_payment ${down_payment}')
