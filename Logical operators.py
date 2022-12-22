72f243b5f86318d39ad16508617fdeb9567af9b5
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-12-22 10:44:16 +0600

    add logical operators

---
+++ Logical operators.py
@@ -0,0 +1,25 @@
+# in and operator both condition should be True
+
+# has_high_income = False
+# has_good_credit = True
+
+# if has_high_income and has_good_credit:
+#     print("Eligible for loan")
+
+
+# in or operator at least one condition should be true
+
+# has_high_income = False
+# has_good_credit = True
+#
+# if has_high_income or has_good_credit:
+#     print("Eligible for loan")
+
+# not operator reverse the boolean value
+
+has_good_credit = True
+has_criminal_record = True
+
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
+
